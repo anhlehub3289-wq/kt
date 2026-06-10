8cbac9b040a3a1639a47f23fafee5003028b6e60
anhlehub3289-wq <anhle.hub.3289@gmail.com>
2026-06-10 16:23:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import io
 
 # Cấu hình trang Streamlit
 st.set_page_config(
-    page_title="Hệ thống phát hiện giao dịch bất thường trong kiểm toán nội bộ",
+    page_title="Hệ thống phát hiện bất thường trong kiểm toán nội bộ",
     page_icon="🏦",
     layout="wide",
     initial_sidebar_state="expanded"
